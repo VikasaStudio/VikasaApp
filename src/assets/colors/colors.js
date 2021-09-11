1dffd091ad3d8717c27d34e75905997735d3ce14
= <=>
2021-09-11 19:57:36 +0530

    basic folders added

---
+++ src/assets/colors/colors.js
@@ -0,0 +1,10 @@
+//Color Palette
+const colors = {
+    background: '#EE6352',
+    text:'#EFE9F4',
+    title:'#484D6D',
+    primary:'#57A773',
+    secondary:'#08B2E3'
+}
+
+export default colors;
