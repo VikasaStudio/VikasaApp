c172fdf771277a83865c8aa2c5e5f066746f33e7
keshav2010 <sharmakeshav15157@gmail.com>
2021-12-10 13:54:50 +0530

    updated config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,8 @@
 export default {
-    'VikasaAPI':'http://10.0.2.2:3000/api'
+    'VikasaAPI':'http://10.0.2.2:3000/api',
+    'SharedPreferenceKeys':{
+        'AccessToken': 'access_token',
+        'RefreshToken': 'refresh_token',
+        'Username': 'username'
+    }
 }
